68696ea7899f3f8659615768519ccabe76ff73f4
Dan Saa <dansaa8@protonmail.com>
2023-01-27 17:54:21 +0100

    Create basic get(method) for index

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import express from "express";
-import fs from "fs/promises";
+import express from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 
-app.get("/", async (request, response) => {
-  const buf = await fs.readFile("./pages/index.html");
-  response.type("html");
-  response.send(buf);
+app.set('view engine', 'ejs');
+
+app.use(express.static('static'));
+
+app.get('/', async (request, res) => {
+  res.render('index', {title: 'Home'});
 });
 
 //första parametern är url, andra parametern är pathen där de statiska filerna ligger.
-app.use("/static", express.static("./static"));
+app.use('/static', express.static('./static'));
 app.use('/pages', express.static('./pages'));
 app.use('/js', express.static('./js'));
 app.listen(5080);
